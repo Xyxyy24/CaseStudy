b6d2cca49380e8d35e442e5c72e883f581a06e11
denmark349 <denimatias1010@gmail.com>
2021-03-19 20:19:20 +0800

    firstcommit

--- Namemethods/NameAge.java
+++ Namemethods/NameAge.java
@@ -1,7 +1,23 @@
 
 public class NameAge {
-  public static void main(String[] args) {
-    
+//age method
+	static void myMethod(String fname, int age) {
+	System.out.println(fname + " is " + age);
+}
+//color method
+	static void newMethod(String fname, String color) {
+	System.out.println(fname + " Favorite color is " + color);
+}
+//main method
+  public static void main(String[] args) 
+{
+    myMethod("Gon", 12);
+    myMethod("Killua", 11);
+    myMethod("Kurapika", 19);
+	System.out.println();
+	newMethod("Gon","Green");
+	newMethod("Killua","White");
+	newMethod("Kurapika","Red");
   }
 }
 
